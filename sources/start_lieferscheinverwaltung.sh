df8eeadd37b9104e0ade88c504d82ed74a025a17
SCRIPTEDAI\User <scripted.AI@gmail.com>
2026-09-16 20:45:36 +0200

    Initialer

---
+++ sources/start_lieferscheinverwaltung.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Starte Lieferscheinverwaltung
+
+echo Check ob Docker aktiv
+
+if systemctl is-active --quiet docker; then
+    echo Starte n8n
+    docker start n8n
+else
+    echo Starte Docker
+    sudo systemctl start docker
+    echo Starte n8n
+    docker start n8n
+fi
+
+npm update whatsapp-web.js
+
+node index.js
